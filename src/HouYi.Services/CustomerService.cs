967fbb2a618bf5c7744e45a2485a8747b8de2ad3
wenhx <3909713+wenhx@users.noreply.github.com>
2025-04-16 21:47:57 +0800

    #10 完成了客户管理列表的搜索功能。

--- src/HouYi.Services/CustomerService.cs
+++ src/HouYi.Services/CustomerService.cs
@@ -1,4 +1,4 @@
-using HouYi.Data;
+﻿using HouYi.Data;
 using HouYi.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,18 +13,61 @@ public class CustomerService : ICustomerService
         _dbContext = dbContext;
     }
 
-    public async Task<PagedResult<Customer>> GetCustomersAsync(int pageNumber = 1, int pageSize = 10)
+    public async Task<PagedResult<Customer>> GetCustomersCoreAsync(Func<IQueryable<Customer>, IQueryable<Customer>>? filter, int pageNumber = 1, int pageSize = 10)
     {
         Utils.NormalizePaginationInputs(ref pageNumber, ref pageSize);
 
-        var query = _dbContext.Customers;
+        var query = _dbContext.Customers.AsQueryable();
+        query = filter != null ? filter(query) : query;
 
         var totalCount = await query.CountAsync();
+        Console.WriteLine("Total Count: " + totalCount);
         var items = await query
             .Skip((pageNumber - 1) * pageSize)
             .Take(pageSize)
             .ToListAsync();
 
         return new PagedResult<Customer>(items, pageNumber, pageSize, totalCount);
+    }
+
+    public async Task<PagedResult<Customer>> GetCustomersAsync(int pageNumber = 1, int pageSize = 10)
+    {
+        return await GetCustomersCoreAsync(filter: null, pageNumber, pageSize);
+    }
+
+    public async Task<PagedResult<Customer>> FindCustomersAsync(string field = "", string term = "", int pageNumber = 1, int pageSize = 10)
+    {
+        return await GetCustomersCoreAsync(filter, pageNumber, pageSize);
+
+        IQueryable<Customer> filter(IQueryable<Customer> query)
+        {
+
+            if (!string.IsNullOrWhiteSpace(term))
+            {
+                term = term.Trim();
+
+                switch (field)
+                {
+                    case "Name":
+                        query = query.Where(c => c.Name.Contains(term));
+                        break;
+                    case "ContactPerson":
+                        query = query.Where(c => c.ContactPerson.Contains(term));
+                        break;
+                    case "Phone":
+                        query = query.Where(c => c.Phone.Contains(term));
+                        break;
+                    case "Email":
+                        query = query.Where(c => c.Email.Contains(term));
+                        break;
+                    case "Address":
+                        query = query.Where(c => c.Address.Contains(term));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return query;
+        }
     }
 }
