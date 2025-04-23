6f83f6ed3342fecb84bc0eb3e81ef3edfb7b2f57
wenhx <3909713+wenhx@users.noreply.github.com>
2025-04-23 21:47:32 +0800

    #43 完成了人才库简历新增和编辑部分代码的优化

---
+++ src/HouYi/Models/ResumeWithIntValue.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace HouYi.Models;
+
+public class ResumeWithIntValue : Resume
+{
+    public int IntAge
+    {
+        get
+        {
+            return Age;
+        }
+        set
+        {
+            Age = (byte)value;
+        }
+    }
+    public int IntAnnualSalary
+    {
+        get
+        {
+            return AnnualSalary;
+        }
+        set
+        {
+            AnnualSalary = (short)value;
+        }
+    }
+    public int IntYearsOfExperience
+    {
+        get
+        {
+            return YearsOfExperience;
+        }
+        set
+        {
+            YearsOfExperience = (byte)value;
+        }
+    }
+
+    [SetsRequiredMembers]
+    public ResumeWithIntValue(Resume resume)
+    {
+        if (resume == null)
+            throw new ArgumentNullException(nameof(resume));
+
+        Id = resume.Id;
+        Name = resume.Name;
+        Gender = resume.Gender;
+        Age = resume.Age;
+        Phone = resume.Phone;
+        Email = resume.Email;
+        Position = resume.Position;
+        Status = resume.Status;
+        HighestEducation = resume.HighestEducation;
+        AnnualSalary = resume.AnnualSalary;
+        YearsOfExperience = resume.YearsOfExperience;
+        PlaceId = resume.PlaceId;
+        Place = resume.Place;
+        Source = resume.Source;
+        Note = resume.Note;
+        CreatedAt = resume.CreatedAt;
+        UpdatedAt = resume.UpdatedAt;
+    }
+}
